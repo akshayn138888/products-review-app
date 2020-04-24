322a8a375059c53f35ab38a859e9204867d1d4ad
Akshay Nanda <akshayn@sfu.ca>
2020-04-24 15:29:19 -0700

    Create and Destroy for reviews completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,16 +10,18 @@ class ReviewsController < ApplicationController
             @reviews = @product.reviews.order(created_at: :desc)
             render 'products/show'
         end
+        
     end
+    
     def destroy 
         @review = Review.find params[:id]
         @review.destroy 
         redirect_to product_path(@review.product)
-    end
+    end 
     
+   ``
     
     private 
-    
     def review_params 
         params.require(:review).permit(:rating,:body)
     end
